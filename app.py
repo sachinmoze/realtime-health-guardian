d8282055ab8c32ce95490737f58eeffb2cc7b810
sachinmoze <mozesachin02@gmail.com>
2024-04-01 17:52:27 +0530

    added render urls for home page

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/onboarding')
+def onboarding():
+    return render_template('onboarding.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
