1f991942b2e2723a59f9a8cdb89e4d6c37c426b5
sachinmoze <mozesachin02@gmail.com>
2024-04-01 19:50:03 +0530

    users database create on local database file

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+import json
+import sqlite3
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['MYSQL_HOST'] = 'health.db'
+app.config['MYSQL_USER'] = 'your_username'
+app.config['MYSQL_PASSWORD'] = 'your_password'
+app.config['MYSQL_DB'] = 'your_database'
+
+mysql = MySQL(app)
+
+conn = sqlite3.connect('health.db')
+
+cursor = conn.cursor()
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    mobilenumber VARCHAR(15),
+    password VARCHAR(100) NOT NULL
+        )
+    """)
+conn.commit()
+conn.close()
 
 @app.route('/')
 def home():
@@ -23,4 +50,4 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
