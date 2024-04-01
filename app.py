990a753c993230f88ede78c3631b8a661d3e1106
sachinmoze <mozesachin02@gmail.com>
2024-04-01 22:38:53 +0530

    signup python logic

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
 import sqlite3
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -14,8 +16,11 @@ app.config['MYSQL_DB'] = 'your_database'
 
 mysql = MySQL(app)
 
-conn = sqlite3.connect('health.db')
 
+
+login_manager = LoginManager(app)
+
+conn = sqlite3.connect('health.db')
 cursor = conn.cursor()
 cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
@@ -30,13 +35,41 @@ cursor.execute("""
 conn.commit()
 conn.close()
 
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        mobile = request.form['mobile']
+        password = request.form['password']
+        print(firstname,lastname,email,mobile,password)
+        ## encripting password
+        hashed_password = generate_password_hash(password)
+
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (firstname, lastname, email, mobilenumber, password) VALUES (%s, %s, %s, %s, %s)",
+                    (firstname, lastname, email, mobile, hashed_password))
+        
+        conn.commit()
+        conn.close()
+
+        flash('Registration successful. Please log in.', 'success')
+        return redirect(url_for('login'))
+
     return render_template('signup.html')
 
 
