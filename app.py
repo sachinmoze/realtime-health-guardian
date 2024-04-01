9f156467ef89269d933dbd914424a67648649d75
sachinmoze <mozesachin02@gmail.com>
2024-04-02 05:15:59 +0530

    signup form python logic change

--- app.py
+++ app.py
@@ -5,26 +5,29 @@ import json
 import sqlite3
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['MYSQL_HOST'] = 'health.db'
-app.config['MYSQL_USER'] = 'your_username'
-app.config['MYSQL_PASSWORD'] = 'your_password'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = 'bablumoze'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'your_database'
 
+#mongodb+srv://bablumoze:<password>@cluster0.oq3mqne.mongodb.net/
+
 mysql = MySQL(app)
-
-
 
 login_manager = LoginManager(app)
 
 conn = sqlite3.connect('health.db')
 cursor = conn.cursor()
 cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     firstname VARCHAR(50),
     lastname VARCHAR(50),
     email VARCHAR(100) UNIQUE,
@@ -34,6 +37,16 @@ cursor.execute("""
     """)
 conn.commit()
 conn.close()
+
+class SignupForm(FlaskForm):
+    firstName = StringField('First Name', validators=[DataRequired()])
+    lastName = StringField('Last Name')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    countryCode = StringField('Country Code', validators=[DataRequired()])
+    mobile = StringField('Mobile Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
 
 class User(UserMixin):
     def __init__(self, user_id):
@@ -50,27 +63,35 @@ def home():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
+    form = SignupForm()
+
+    if form.validate_on_submit():
+        # Process form data (e.g., save to database)
+        firstname = request.form['firstName']
+        lastname = request.form['lastName']
         email = request.form['email']
+        countryCode = request.form['countryCode']
         mobile = request.form['mobile']
         password = request.form['password']
-        print(firstname,lastname,email,mobile,password)
-        ## encripting password
-        hashed_password = generate_password_hash(password)
-
-        cur = conn.cursor()
-        cur.execute("INSERT INTO users (firstname, lastname, email, mobilenumber, password) VALUES (%s, %s, %s, %s, %s)",
-                    (firstname, lastname, email, mobile, hashed_password))
         
+        hashed_password = generate_password_hash(password)
+        
+        conn = sqlite3.connect('health.db')
+        cursor = conn.cursor()
+        # cursor.execute("INSERT INTO users (firstname, lastname, email, mobilenumber, password) VALUES (%s, %s, %s, %s, %s)",
+        #             (firstname, lastname, email, countryCode + mobile, hashed_password))
+        cursor.execute("INSERT INTO users (firstname, lastname, email, mobilenumber, password) VALUES (?, ?, ?, ?, ?)",
+                       (firstname, lastname, email, mobile, hashed_password))
+        
+
         conn.commit()
         conn.close()
-
+        
         flash('Registration successful. Please log in.', 'success')
         return redirect(url_for('login'))
 
-    return render_template('signup.html')
+    return render_template('signup.html', form=form)
+
 
 
 @app.route('/onboarding')
