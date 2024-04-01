c32b2c473f8ac52d4e0aff7bf1dfe375d09f72f7
sachinmoze <mozesachin02@gmail.com>
2024-04-01 17:34:51 +0530

    app home page setup

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
